0a5f34cbd65359244681acbb096b770dd0c407c4
Rehan <rali7196@gmail.com>
2025-10-22 15:33:19 -0700

    another commit

--- web_app/src/App.tsx
+++ web_app/src/App.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 function App() {
   const [userName, setUserName] = useState<String>("");
   const [password, setPassword] = useState<String>("");
-  //
+
   return (
     <>
       <div>
