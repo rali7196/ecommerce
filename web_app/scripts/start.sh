2b1ecce93e564a806dd9c4dd12a4c756b6dcb84b
Rehan <rali7196@gmail.com>
2025-10-22 15:31:40 -0700

    initial commit

---
+++ web_app/scripts/start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+nvm use 22.14
+npm run dev
---
+++ web_app/src/App.tsx
@@ -0,0 +1,28 @@
+import { TextField, Button } from "@mui/material";
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [userName, setUserName] = useState<String>("");
+  const [password, setPassword] = useState<String>("");
+
+  return (
+    <>
+      <div>
+        <TextField
+          placeholder={"Username"}
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+        ></TextField>
+        <TextField
+          placeholder={"Password"}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        ></TextField>
+        <Button>Log In</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
